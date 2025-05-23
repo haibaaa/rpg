29e188a60be1d10683c2f29db913543f0b174dc8
bjowb <pulktchhr@gmail.com>
2025-05-23 23:35:58 +0530

    static overworld initialized!

---
+++ init.js
@@ -0,0 +1,9 @@
+(function () {
+    // console.log("This is working!");
+
+    const overworld = new Overworld({
+        element: document.querySelector(".game-container")
+    });
+
+    overworld.init();
+})();
